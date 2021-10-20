fbb4750e497a23ea54de3ffa4c89c03bfcd512ae
Lyra Naeseth <lyra@lyra.codes>
2021-10-20 11:36:51 -0700

    Add temporary EGL debug output

--- src/native/webgl.cc
+++ src/native/webgl.cc
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <cstring>
 #include <vector>
 #include <iostream>
@@ -63,6 +64,11 @@ WebGLRenderingContext::WebGLRenderingContext(
       state_message = ERROR_EGL_GET_DISPLAY;
       return;
     }
+
+    fprintf(stderr, "EGL_VENDOR:      %s\n", eglQueryString(DISPLAY, EGL_VENDOR));
+    fprintf(stderr, "EGL_VERSION:     %s\n", eglQueryString(DISPLAY, EGL_VERSION));
+    fprintf(stderr, "EGL_CLIENT_APIS: %s\n", eglQueryString(DISPLAY, EGL_CLIENT_APIS));
+    fprintf(stderr, "EGL_EXTENSIONS:  %s\n", eglQueryString(DISPLAY, EGL_EXTENSIONS));
 
     //Initialize EGL
     if (!eglInitialize(DISPLAY, NULL, NULL)) {
